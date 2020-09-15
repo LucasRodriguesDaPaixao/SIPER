e5001d5345b38e26e1d456973f6709d81277273b
Gabriel52 <gabrielbrune52@gmail.com>
2020-09-15 15:50:40 -0300

    update connection database

--- SIPER/src/factory/ConnecrionFactory.java
+++ SIPER/src/factory/ConnecrionFactory.java
@@ -5,10 +5,26 @@
  */
 package factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Gabriel Brune
  */
 public class ConnecrionFactory {
-    
+  public static Connection getConnection(){
+        final String myServer = "jdbc:mysql://127.0.0.1:3306/siper";
+        final String user = "root";
+        final String psw = "";
+        
+        try{
+            return DriverManager.getConnection(myServer, user, psw);
+            
+        }catch(SQLException error){
+            throw new RuntimeException(error);
+            
+        }
+    }   
 }
